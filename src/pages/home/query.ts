15e9d291208ec70a51af1ff5004f06be180b109c
fendishatech <liyukibir@gmail.com>
2024-02-02 10:55:35 -0800

    protect api key

--- src/pages/home/query.ts
+++ src/pages/home/query.ts
@@ -1,31 +1,29 @@
 import axios from "axios";
 
+const POPULAR_MOVIES_URL =
+  "https://api.themoviedb.org/3/movie/popular?language=en-US&page=1";
+const POPULAR_TV_SHOWS_URL =
+  "https://api.themoviedb.org/3/tv/popular?language=en-US&page=1";
+const TMDB_ACCESS_TOKEN = import.meta.env.VITE_APP_TMDB_ACCESS_TOKEN;
+
 export const fetchMovies = async () => {
-  const res = await axios.get(
-    "https://api.themoviedb.org/3/movie/popular?language=en-US&page=1",
-    {
-      headers: {
-        accept: "application/json",
-        Authorization:
-          "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiNTFlNDg3NGUyNGQ5ZTBiZmYwZGZmODNkZTA5NDE4MSIsInN1YiI6IjYxZjY4M2U2ZTlkYTY5MDBjNDI4YmUxOSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.LvRDjbtLUKrpi5-6y70LDOfILCUI24O8fsZGPXvNiSg",
-      },
-    }
-  );
+  const res = await axios.get(POPULAR_MOVIES_URL, {
+    headers: {
+      accept: "application/json",
+      Authorization: `Bearer ${TMDB_ACCESS_TOKEN}`,
+    },
+  });
 
   return res.data;
 };
 
 export const fetchTvShows = async () => {
-  const res = await axios.get(
-    "https://api.themoviedb.org/3/tv/popular?language=en-US&page=1",
-    {
-      headers: {
-        accept: "application/json",
-        Authorization:
-          "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiNTFlNDg3NGUyNGQ5ZTBiZmYwZGZmODNkZTA5NDE4MSIsInN1YiI6IjYxZjY4M2U2ZTlkYTY5MDBjNDI4YmUxOSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.LvRDjbtLUKrpi5-6y70LDOfILCUI24O8fsZGPXvNiSg",
-      },
-    }
-  );
+  const res = await axios.get(POPULAR_TV_SHOWS_URL, {
+    headers: {
+      accept: "application/json",
+      Authorization: `Bearer ${TMDB_ACCESS_TOKEN}`,
+    },
+  });
 
   return res.data;
 };
